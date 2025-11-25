1c411eb555868f9df3a9f5e5381dc90a1db6d186
Yash Gupta <76595447+conqryash007@users.noreply.github.com>
2025-11-25 19:50:43 +0530

    added new blog

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -6,5 +6,13 @@ export const blogs = [
     image: "https://i.ibb.co/d0z8t8TX/Chat-GPT-Image-Nov-25-2025-11-12-10-AM.png", 
     description: "A deep breakdown of the engineering-first process Quagnitia uses to take Web3 ideas from concept to deployed, production-grade systems.",
     date: "November 25, 2025"
+  },
+  {
+    id: 2,
+    slug: "how-to-launch-a-cryptocurrency-token-in-2025",
+  title: "How to Launch a Cryptocurrency Token in 2025: Step-by-Step Guide",
+  image: "https://i.ibb.co/sdvrNQTL/Chat-GPT-Image-Nov-25-2025-05-33-21-PM.png",
+  description: "A precise 2025 playbook for launching a cryptocurrency token—covering purpose, blockchain choice, tokenomics, smart contract audits, compliance, infrastructure, and post-launch growth.",
+  date: "November 25, 2025"
   }
 ];
